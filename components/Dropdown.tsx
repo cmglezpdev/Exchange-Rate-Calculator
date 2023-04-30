e9fca99fb87a11ab8c4c21ec35b7d2603e9ff9ae
Carlos Manuel González <cmglezpdev@gmail.com>
2023-04-29 22:09:03 -0400

    feat: add dropdown

---
+++ components/Dropdown.tsx
@@ -0,0 +1,15 @@
+import { StyledDropdown } from "./styled-components"
+
+
+export const Dropdown = () => {
+    return (
+        <StyledDropdown>
+            <div>Select</div>
+            <ul>
+                <li>USD</li>
+                <li>MLC</li>
+                <li>CUP</li>
+            </ul>
+        </StyledDropdown>
+    )
+}
--- components/styled-components/Dropdown.ts
+++ components/styled-components/Dropdown.ts
@@ -1,15 +1,38 @@
 import styled from 'styled-components';
 
 
-export const Dropdown = styled.select`
-    width: 100%;
-    border-radius: 5px;
-    padding: 20px 5px;
-    font-weight: bold;
-`;
+export const StyledDropdown = styled.div`
+    & > div {
+        width: 100%;
+        border: 1px solid #000;
+        border-radius: 5px;
+        padding: 15px 10px;
+        font-weight: bold;
+        font-size: 20px;
+    }
 
-export const Option = styled.option`
-    width: 100%;
-    padding: 20px 5px;
-    font-weight: bold;
-`
+    & > ul {
+        display: none;
+        position: absolute;
+        list-style: none;
+        width: 190px;
+    }
+
+    &:hover > ul {
+        display: block;
+    }
+
+    & > ul > li {
+        width: 100%;
+        padding: 15px 10px;
+        border-left: 1px solid #000;
+        border-right: 1px solid #000;
+        border-bottom: 1px solid #000;
+        background-color: #fff;
+    }
+
+    &:hover > ul > li:hover {
+        background-color: red;
+        color: blue;
+    }
+`;
